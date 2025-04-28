e172ad6e261fae2a1a0f1e9464584675239ba89a
harvard2025 <prro.code@gmail.com>
2025-04-28 19:53:09 +0300

    Draft error

--- 1.bat
+++ 1.bat
@@ -2,4 +2,8 @@ color 04
 git config --global --add safe.directory 'D:/proggraming/Web Development/PrroCoders/V5/PrroCoders'
 
 git remote add origin https://github.com/harvard2025/PrroCoders.git
+git add .
 
+git commit -m 'V7 - Store 1 Page Half Blog'
+
+git push -u
